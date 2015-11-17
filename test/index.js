aa0f7eac676ad748cc92496b56fec3295ee640d2
queckezz <fabian.eichenberger@gmail.com>
2015-11-17 19:18:59 +0100

    test: mockRegistry, test windows specific functions only when on windows

--- test/index.js
+++ test/index.js
@@ -1,17 +1,20 @@
 
 import test from 'ava'
 import 'babel-core/register'
-import { getUserEnv, getSystemEnv } from '../src'
+import { getUserEnv, getSystemEnv, parseRegistry } from '../src'
 import { find, identical } from 'ramda'
+import { platform } from 'os'
 
-test('get user env variables', t => testEnv(t, getUserEnv))
+test('parse registry', t => testEnv(t, mockRegistry))
 
-test('get system env variables', t => testEnv(t, getSystemEnv))
+if (isWindows()) {
+  test('get user env variables', t => testEnv(t, getUserEnv))
+  test('get system env variables', t => testEnv(t, getSystemEnv))
+}
 
 function testEnv (t, getEnv) {
   return getEnv()
   .then((registry) => {
-    console.log(registry)
     t.ok(findKey('TEMP', registry))
   })
 }
@@ -21,4 +24,21 @@ function findKey (key, registry) {
     ({ name }) => identical(key, name),
     registry
   )
+}
+
+function mockRegistry () {
+  const str = `
+  HKEY_CURRENT_USER\\Environment
+    TEMP    REG_EXPAND_SZ    %USERPROFILE%\\AppData\\Local\\Temp
+    TMP    REG_EXPAND_SZ    %USERPROFILE%\\AppData\\Local\\Temp
+    Path    REG_EXPAND_SZ    C:\\Users\\someuser\\AppData\\Local\\atom\\bin;%NVM_HOME%;%NVM_SYMLINK%
+    NVM_HOME    REG_EXPAND_SZ    C:\\Users\\someuser\\Tools\\nvm
+    NVM_SYMLINK    REG_EXPAND_SZ    C:\\Program Files\\nodejs
+  `
+
+  return Promise.resolve(str).then((registry) => parseRegistry(registry))
+}
+
+function isWindows () {
+  return platform() === 'win32'
 }
